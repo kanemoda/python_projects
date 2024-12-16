ccb4fc97f6f982049977341f234bfc7f0ee97b41
kanemoda <efedenizbaglar45@gmail.com>
2024-12-16 19:07:48 +0300

    Bug Fixes

--- Calculator_App/demo.py
+++ Calculator_App/demo.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import ast
 
 root = Tk()
 
@@ -21,11 +22,35 @@ def clear_all():
     display.delete(0,END)
     x = 0
     
+def calculate():
+    entire_string = display.get()
 
+    try:
+        node = ast.parse(entire_string , mode = "eval")
+        result = eval(compile(node ,'<string>' , 'eval' ))
+        clear_all()
+        display.insert(0,result)
+    except Exception:
+        clear_all()
+        display.insert(0,"Error")
 
+def delete(keywords):
+    entire_string = display.get()
 
-
-
+    if (entire_string.endswith(tuple(keywords))):
+        for sub in keywords:
+            if(entire_string.endswith(sub)):
+                entire_string = entire_string[:-len(sub)]
+                clear_all()
+                display.insert(0,entire_string)
+                break
+    elif len(entire_string):
+        new_string = entire_string[:-1]
+        clear_all()
+        display.insert(0,new_string)
+    else:
+        clear_all()
+        display.insert(0 , "")
 
 display = Entry(root)
 display.grid(row=1 , columnspan=6)
@@ -43,7 +68,7 @@ for i in range(3):
 button = Button(root , text="0" , height=3 , width=3 , command=lambda:get_number(0))
 button.grid(row=5 , column=1)
 
-operations = ["+" , "-" , "*" , "/" , "*3.14" , "%" , "(" , "**" ,")" , "**2"]
+operations = ["+" , "-" , "*" , "/" , "*3.14" , "%" , "(" , "**" ,")" , "**2" , "."]
 
 count = 0
 for i in range(4):
@@ -55,7 +80,8 @@ for i in range(4):
             button.grid(row=i+2 , column=j + 6)
 
 Button(root , text="AC" , width=3 , height=3 , command=lambda:clear_all()).grid(row = 5 , column=0)
-Button(root , text="=" , width=3 , height=3).grid(row = 5 , column=2)
+Button(root , text="=" , width=3 , height=3 , command=lambda:calculate()).grid(row = 5 , column=2)
+Button(root , text= "<-" , width=3 , height=3,command=lambda:delete(operations)).grid(row=5 , column=8)
 
 
 root.mainloop()
